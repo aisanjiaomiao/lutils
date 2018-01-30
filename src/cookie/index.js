2b4df0731adfae182cf0da505bf83991045b525e
宝具滑 <1002562804@qq.com>
2018-01-30 18:22:02 +0800

    Upload cookie/getCookie.js cookie/index.js cookie/setCookie.js cookie/removeCookie.js

---
+++ src/cookie/index.js
@@ -0,0 +1,7 @@
+const getCookie = require('./getCookie');
+const removeCookie = require('./removeCookie');
+const setCookie = require("./setCookie")
+
+module.exports = {
+    getCookie, removeCookie, setCookie
+};
