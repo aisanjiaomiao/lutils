3b9935ce424a8dc2880169f039912091c0387f41
宝具滑 <1002562804@qq.com>
2018-01-30 23:31:11 +0800

    Upload url/index.js url/getQueryString.js url/stringfyQueryString.js url/getQueryObject.js

---
+++ src/url/getQueryString.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @desc   通过name获取url参数
+ * @param  {String} url  default: window.location.href
+ * @return {Object} 
+ */
+function getQueryString(name) {//获取url参数值方法2
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);Object
+    if (r != null) {
+        return decodeURI(r[2]);
+    }
+    return null;
+}
+module.exports = getQueryString
---
+++ src/url/index.js
@@ -0,0 +1,8 @@
+
+const getQueryObject = require('./getQueryObject');
+const stringfyQueryString = require("./stringfyQueryString");
+const getQueryString = require("./getQueryString");
+
+module.exports = {
+    getQueryObject, stringfyQueryString, getQueryString
+};
