bfe4651756eae6827cbbc7bea994ca542edf2100
宝具滑 <1002562804@qq.com>
2018-01-30 22:04:05 +0800

    Upload device/isWeixin.js device/index.js device/getExplore.js device/getOS.js

---
+++ src/device/index.js
@@ -0,0 +1,7 @@
+const getExplore = require('./getExplore');
+const getOS = require('./getOS');
+const isWeixin = require("./isWeixin")
+
+module.exports = {
+    getExplore, getOS, isWeixin
+};
---
+++ src/device/isWeixin.js
@@ -0,0 +1,11 @@
+/**
+ * @desc 是否是微信平台
+ * @return {Boolean} 
+ */
+function isWeixin(){
+    var ua = navigator.userAgent.toLowerCase();
+    return (ua.indexOf('micromessenger') != -1);
+}
+
+
+module.exports = isWeixin;
