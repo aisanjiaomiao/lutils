1b41ae3f6311899348697356a1ef9283fdce514a
宝具滑 <1002562804@qq.com>
2018-01-31 09:47:06 +0800

    Upload xml/json2xml.js xml/xml2json.js xml/index.js xml/parseXml.js

---
+++ src/xml/index.js
@@ -0,0 +1,7 @@
+const json2xml = require('./json2xml');
+const parseXml = require("./parseXml");
+const xml2json = require("./xml2json");
+
+module.exports = {
+    json2xml, xml2json, parseXml
+};
---
+++ src/xml/parseXml.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @desc   字符串转Xml格式Dom 
+ * @param  {String} 缩进空格数量
+ * @return {Element} 
+ */
+function parseXml(xml) {
+    var dom = null;
+    if (window.DOMParser) {
+        try {
+            dom = (new DOMParser()).parseFromString(xml, "text/xml");
+        }
+        catch (e) { dom = null; }
+    }
+    else if (window.ActiveXObject) {
+        try {
+            dom = new ActiveXObject('Microsoft.XMLDOM');
+            dom.async = false;
+            if (!dom.loadXML(xml)) // parse error ..
+                window.alert(dom.parseError.reason + dom.parseError.srcText);
+        }
+        catch (e) { dom = null; }
+    }
+    else
+        alert("oops");
+    return dom;
+}
+
+module.exports = parseXml;
