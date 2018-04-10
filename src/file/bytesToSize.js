c80bfe9b9c5fef50409dc5fbd83a71d185489b21
宝具滑 <1002562804@qq.com>
2018-04-10 16:27:29 +0800

    新建 bytesToSize.js

---
+++ src/file/bytesToSize.js
@@ -0,0 +1,14 @@
+/**  
+ * 计算文件大小 
+ * @param {Number} bytes 文件二进制大小 
+ * @return {String} 
+ */
+function bytesToSize(bytes) {
+    if (bytes === 0) return '0 B';
+    var k = 1024;
+    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    var i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];// 后面保留一位小数，如1.0GB                                                                                                                  //return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];  
+}
+
+module.exports = bytesToSize;
