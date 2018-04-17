7f4ad7370ec33da5b81225597a5f3b93a9055ac0
宝具滑 <1002562804@qq.com>
2018-04-17 15:42:29 +0800

    更新 index.js

--- src/file/index.js
+++ src/file/index.js
@@ -1,8 +1,9 @@
 const toBlobURL = require('./toBlobURL');
 const toDataURL = require('./toDataURL');
 const base64UrlToBlob=require("./base64UrlToBlob"); 
+const base64UrlToFile=require("./base64UrlToFile"); 
 const downloadByUrl=require("./downloadByUrl");
 const bytesToSize=require('./bytesToSize');
 module.exports = {
-    toBlobURL, toDataURL,base64UrlToBlob,downloadByUrl,bytesToSize
+    toBlobURL, toDataURL,base64UrlToBlob,downloadByUrl,bytesToSize,base64UrlToFile
 };
