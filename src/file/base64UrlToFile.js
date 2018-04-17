eb44d8db3a00b1f5d10a7734f04f724afcb8f565
宝具滑 <1002562804@qq.com>
2018-04-17 15:44:50 +0800

    新建 base64UrlToFile.js

---
+++ src/file/base64UrlToFile.js
@@ -0,0 +1,20 @@
+/**  
+ * 将以base64的图片url数据转换为Blob  
+ * @param {String} urlData   用url方式表示的base64图片数据 
+ * @param {String} name 文件名
+ * @param {String} type 文件类型 
+ * @return {File} 
+ */
+function base64UrlToFile(urlData, name, type) {
+    var bytes = window.atob(urlData.split(',')[1]);
+    type = type || i.img.split(";")[0].split(":")[1];
+    var ab = new ArrayBuffer(bytes.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < bytes.length; i++) {
+        ia[i] = bytes.charCodeAt(i);
+    }
+    return (new File([ab], name, { 'type': type}));
+}
+
+
+module.exports = base64UrlToFile;
