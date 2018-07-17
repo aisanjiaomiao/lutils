9dba83c9b280942d3c96fd3cb38ea6a3b74cc1f6
宝具滑 <1002562804@qq.com>
2018-07-17 16:01:20 +0800

    更新 getTimeStamp.js

--- src/date/getTimeStamp.js
+++ src/date/getTimeStamp.js
@@ -1,7 +1,8 @@
 /**
- * 返回一个时间戳,以毫秒为单位,精确到千分之一毫秒
- *@return {Number}
+ * 返回一个时间戳,
+ * @param {Boolean} n 是否以毫秒为单位,精确到千分之一毫秒
+ * @return {Number}
 */
-function getTimeStamp() {
-    return performance.now();
+function getTimeStamp(n) {
+    return n ? performance.now() : (+new Date());
 }
