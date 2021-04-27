ca204b1469a902fb7bbfbcd8f7ed7431cefb60b5
宝具华 <1002562804@qq.com>
2021-04-27 11:25:28 +0800

    change:clipboard

--- dist/2.0.2/lutils.js
+++ dist/2.0.2/lutils.js
@@ -1366,23 +1366,43 @@ var lutils = (function () {
 
     /**
      * 
+     * @desc   写入剪切板 老版
+     * @param  {String}  
+     */
+    // function clipboard(data){
+    //     data=data.toString();
+    //     let f = document.createElement("form");
+    //     f.id = "copy-" + Date.parse(new Date());
+    //     f.onsubmit = function () { return false };
+    //     f.style = "opacity: 0;height: 1px;width: 1px;overflow: hidden;position:fixed;top: -1;left: -1;z-index: -1;"
+    //     f.innerHTML = `<button onclick='story.select();document.execCommand("Copy");'></button>
+    //         <textarea name="story">${data}</textarea>`;
+    //     document.body.appendChild(f);
+    //     document.querySelector('#' + f.id + '>button').click();
+    //     document.body.removeChild(document.getElementById(f.id));
+    // }
+
+    /**
+     * 
      * @desc   写入剪切板
      * @param  {String}  
      */
-    function clipboard(data) {
-      data = data.toString();
-      var f = document.createElement("form");
-      f.id = "copy-" + Date.parse(new Date());
+    function clipboard(str) {
+      var el = document.createElement('textarea');
+      el.value = str;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+      var selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
 
-      f.onsubmit = function () {
-        return false;
-      };
-
-      f.style = "opacity: 0;height: 1px;width: 1px;overflow: hidden;position:fixed;top: -1;left: -1;z-index: -1;";
-      f.innerHTML = "<button onclick='story.select();document.execCommand(\"Copy\");'></button>\n        <textarea name=\"story\">".concat(data, "</textarea>");
-      document.body.appendChild(f);
-      document.querySelector('#' + f.id + '>button').click();
-      document.body.removeChild(document.getElementById(f.id));
+      if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
+      }
     }
 
     /**
--- src/clipboard.js
+++ src/clipboard.js
@@ -1,18 +1,39 @@
 /**
  * 
+ * @desc   写入剪切板 老版
+ * @param  {String}  
+ */
+// function clipboard(data){
+//     data=data.toString();
+//     let f = document.createElement("form");
+//     f.id = "copy-" + Date.parse(new Date());
+//     f.onsubmit = function () { return false };
+//     f.style = "opacity: 0;height: 1px;width: 1px;overflow: hidden;position:fixed;top: -1;left: -1;z-index: -1;"
+//     f.innerHTML = `<button onclick='story.select();document.execCommand("Copy");'></button>
+//         <textarea name="story">${data}</textarea>`;
+//     document.body.appendChild(f);
+//     document.querySelector('#' + f.id + '>button').click();
+//     document.body.removeChild(document.getElementById(f.id));
+// }
+/**
+ * 
  * @desc   写入剪切板
  * @param  {String}  
  */
-function clipboard(data){
-    data=data.toString();
-    let f = document.createElement("form");
-    f.id = "copy-" + Date.parse(new Date());
-    f.onsubmit = function () { return false };
-    f.style = "opacity: 0;height: 1px;width: 1px;overflow: hidden;position:fixed;top: -1;left: -1;z-index: -1;"
-    f.innerHTML = `<button onclick='story.select();document.execCommand("Copy");'></button>
-        <textarea name="story">${data}</textarea>`;
-    document.body.appendChild(f);
-    document.querySelector('#' + f.id + '>button').click();
-    document.body.removeChild(document.getElementById(f.id));
+function clipboard(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    const selected =document.getSelection().rangeCount > 0? document.getSelection().getRangeAt(0): false;
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
+    }
 }
-export default   clipboard;
+export default clipboard;
