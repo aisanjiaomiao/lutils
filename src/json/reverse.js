0c732ba2f75f8b4597f6ed4117ed99ca906851eb
爱动手的三角喵 <1002562804@qq.com>
2022-03-04 07:30:52 +0000

    update src/json/reverse.js.

--- src/json/reverse.js
+++ src/json/reverse.js
@@ -4,14 +4,14 @@
  * @return  {Object}       
  */
 function reverse(obj) {
-    if (typeof obj == 'object') {
-        if (Array.isArray(obj))return obj.reverse();
-        var o = new Object();
-        for (var k in obj) {
-            o[obj[k]] = k;
-        }
-        return o;
-    }
+    if (typeof obj != 'object') return obj
+    if (Array.isArray(obj))return obj.reverse();
+    return  Object.fromEntries(Object.entries(obj).map(([a,b])=>[b,a]))
+    //  var o = new Object();
+    //  for (var k in obj) {
+    //      o[obj[k]] = k;
+    //  }
+    //  return o;
 }
 
 export default   reverse
